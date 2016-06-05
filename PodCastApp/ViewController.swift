7a7fae112b5cf8097beea52089cc0388077c1225
Abhishek Karale <k.abhishek@hotmail.co.uk>
2016-06-05 12:37:01 +0100

    Initial Commit

---
+++ PodCastApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PodCastApp
+//
+//  Created by Abhishek Karale on 05/06/2016.
+//  Copyright © 2016 AppMonkey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
